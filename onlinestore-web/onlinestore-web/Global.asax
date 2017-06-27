9858c6bea87dba777cd2b37c8f9d3e6ac73082b2
tcs-ms-poc <sreeraj.ar@outlook.com>
2017-06-27 13:01:37 -0400

    Base code

---
+++ onlinestore-web/onlinestore-web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="onlinestore_web.MvcApplication" Language="C#" %>
---
+++ onlinestore-web/onlinestore-web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(onlinestore_web.Startup))]
+namespace onlinestore_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
